c54efff6a4ea17618741676f5d2656d27cb4e7a0
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-11-26 09:54:36 +0530

    Initial Commit

---
+++ DATA  TRANSFER THRU APP DELEGATE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATA  TRANSFER THRU APP DELEGATE
+//
+//  Created by Syed.Reshma Ruksana on 26/11/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
